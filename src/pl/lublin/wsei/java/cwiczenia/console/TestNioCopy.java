d0600e0909726bbd0f3c9c4b7c3a60f52d8b4c7f
lukasz <lukaszflorek22@wp.pl>
2021-06-09 13:05:44 +0200

    Modyfikacja kodu programu poprzez bardziej stosowaną metodę.

--- src/pl/lublin/wsei/java/cwiczenia/console/TestNioCopy.java
+++ src/pl/lublin/wsei/java/cwiczenia/console/TestNioCopy.java
@@ -1,5 +1,6 @@
 package pl.lublin.wsei.java.cwiczenia.console;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -11,9 +12,9 @@ public class TestNioCopy {
         long startTime = System.currentTimeMillis();
         //long startTime = System.nanoTime(); //dokładniejszy pomiar
 
-        Path oryginal = Paths.get("sample_1920×1280.tiff");
-        Path kopia = Paths.get("img_copy2.tiff");
-        Files.copy(oryginal, kopia);
+        File oryginal = new File("sample_1920×1280.tiff");
+        File kopia = new File("img_copy2.tiff");
+        Files.copy(oryginal.toPath(), kopia.toPath(),StandardCopyOption.REPLACE_EXISTING);
 
         System.out.println("Czas kopiowania: " + (System.currentTimeMillis() - startTime) + " ms");
         //System.out.println("Czas kopiowania: " + (float)(System.nanoTime() - startTime)/1000000 + " ms");
